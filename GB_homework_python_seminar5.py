120cc71a3729700e20995de3a094d2a411214a2e
Serega S <serega1067@yandex.ru>
2022-06-07 22:52:58 +0700

    Task 35 done add file_task35.txt

--- GB_homework_python_seminar5.py
+++ GB_homework_python_seminar5.py
@@ -9,7 +9,7 @@
 многочлен степени k. 
 *Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 '''
-
+'''
 from random import randint, choice
 k = int(input("Введите степень многочлена: "))
 variable = "x"
@@ -48,4 +48,37 @@ print(result)
 
 data = open("file_task33.txt", "a")
 data.write(result + "\n")
+data.close()
+'''
+
+'''
+Задача 35.
+В файле находится N натуральных чисел, записанных через пробел. 
+Среди чисел не хватает одного, чтобы выполнялось условие 
+A[i] - 1 = A[i-1]. Найти его.
+'''
+
+print("\nПроверим, что в файле file_task35.txt порядок чисел\n"
+      "соответствует условию A[i] - 1 = A[i-1] и если это не так\n"
+      "найдём нужное число и исправим несоответствие\n")
+
+data = open("file_task35.txt", "r+")
+data_line = data.readline()
+print("Данные полученные из файла:", data_line)
+
+def file_check(arg_text):
+    arg_text = list(map(int, arg_text.split()))
+    for i in range(1, len(arg_text)):
+        if arg_text[i] - 1 != arg_text[i-1]:
+            arg_text.insert(i, arg_text[i-1] + 1)
+            print(f"Нехватает числа: {arg_text[i-1] + 1}")
+            print("Получился список: ", *arg_text)
+            break
+    result = " ".join(map(str, arg_text))
+    return result
+
+new_data = file_check(data_line)
+data.writelines("\n" + new_data)
+print("Результат записан в файл file_task35.txt\n")
+
 data.close()
